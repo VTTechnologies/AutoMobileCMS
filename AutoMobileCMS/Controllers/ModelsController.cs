993dc3b928be00795c914c460f4b0b3c2f25143a
skafrozvtt <skafrozvtt@gmail.com>
2019-06-12 15:17:13 +0530

    upload image partial view and images to template

--- AutoMobileCMS/Controllers/ModelsController.cs
+++ AutoMobileCMS/Controllers/ModelsController.cs
@@ -25,14 +25,14 @@ namespace AutoMobileCMS.Controllers
         }
         public ActionResult Create()
         {
-            var cities = _brandservice.Get(c => c.Status == true);
-            TempData["brands"] = cities.Select(s => new SelectListItem { Text = s.BrandName, Value = s.BrandID.ToString() }).ToList();
+            //var cities = _brandservice.Get(c => c.Status == true);
+            //TempData["brands"] = cities.Select(s => new SelectListItem { Text = s.BrandName, Value = s.BrandID.ToString() }).ToList();
             return View();
         }
         [HttpPost]
         public ActionResult Create(ModelsViewModel modelsviewmodel)
         {
-            return View();
+            return PartialView("UploadImage");
         }
     }
 }
